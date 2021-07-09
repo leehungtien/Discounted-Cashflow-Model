0acfc82b9782984c5d1c13d9ab5b0af88008e0e3
leehungtien <leehungtien@gmail.com>
2021-07-10 00:04:51 +0800

    Added logic to calculate average debt rate

--- DCF.py
+++ DCF.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import yfinance as yf
 import datetime
+from statistics import mean
 
 # Global variable for years of projection
 YEARS = 3
@@ -17,8 +18,8 @@ netIncome = list(map(lambda x: int(x), input("Key in Net Income of the format 'C
 # Calculate Free Cash Flow to Net Income Ratio
 FCF_NI_ratios = []
 for i in range(len(cashFlow)):
-    FCF_NI_ratios.append(netIncome[i] / cashFlow[i])
-FCF_NI = 1 + min(FCF_NI_ratios)
+    FCF_NI_ratios.append(netIncome[i + 1] / cashFlow[i])
+FCF_NI = min(FCF_NI_ratios)
 
 # Prompt for Total Revenue (Can be obtained on Yahoo Finance)
 totalRevenue = input("Key in Total Revenue of the format 'Current Year - 3 Current Year - 2 Current Year - 1 Analyst Estimates': ").split()
@@ -26,8 +27,8 @@ totalRevenue = input("Key in Total Revenue of the format 'Current Year - 3 Curre
 # Calculate Revenue Growth Rate
 growthRates = []
 for i in range(len(totalRevenue)):
-    growthRates.append(1 - totalRevenue[i + 1] / totalRevenue[i])
-growthRate = 1 + min(growthRates)
+    growthRates.append(totalRevenue[i + 1] / totalRevenue[i])
+growthRate = min(growthRates)
 
 # Project Total Revenue for YEARS
 for i in range(YEARS):
@@ -36,7 +37,7 @@ for i in range(YEARS):
 # Calculate Net Income Margins
 NI_margins = []
 for i in range(len(netIncome)):
-    NI_margins.append(netIncome[i] / totalRevenue[i])
+    NI_margins.append(netIncome[i + 1] / totalRevenue[i])
 NI_margin = 1 + min(NI_margins)
 
 # Projected Net Income
@@ -46,3 +47,25 @@ for i in reversed(range(1, YEARS + 1)):
 # Project Free Cash Flow
 for i in reversed(range(1, YEARS + 1)):
     cashFlow.append(netIncome[-i] * FCF_NI)
+
+# Weighted Average Cost to Capital
+def weighedAverageCostToCaptial():
+    totalDebts = []
+    debtRates = []
+
+    # Prompt User for Interest Expense (Can be obtained from Income Statement on Yahoo Finance)
+    interestExpense = int(input("Enter Interest Expense from Income Statement: "))
+
+    # Prompt User for Current Debt (Can be obtained from Balance Sheet on Yahoo Finance)
+    currentDebt = int(input("Enter Current Debt from Balance Sheet: "))
+
+    # Prompt User for Long Term Debt (Can be obtained from Balance Sheet on Yahoo Finance)
+    longTermDebt = int(input("Enter Long Term Debt from Balance Sheet: "))
+
+    totalDebts.append(currentDebt + longTermDebt) 
+    for i in range(len(totalDebts)):
+        if i < len(totalDebts):
+            debtRates.append(totalDebts[i + 1] / totalDebts[i])
+
+    debtRate = mean(debtRates)
+    
