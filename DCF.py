c0717f014221ad943bc5ea17620dee2c21c867c1
leehungtien <leehungtien@gmail.com>
2021-07-11 20:07:18 +0800

    Added code to calculate intrinisc value

--- DCF.py
+++ DCF.py
@@ -10,23 +10,27 @@ YEARS = 3
 company = str(input("Please enter company's ticker symbol: "))
 
 # Obtain Free Cash Flow Data from user (Can be obtained on Yahoo Finance)
-cashFlow = list(map(lambda x: int(x), input("Key in Free Cash Flow of the format 'Current Year - 3 Current Year - 2 Current Year - 1': ").split()))
+cashFlow = list(map(lambda x: float(x), input("Key in Free Cash Flow of the format 'Current Year - 3 Current Year - 2 Current Year - 1': ").split()))
 
 # Obtain Net Income Data from user (Can be obtained on Yahoo Finance)
-netIncome = list(map(lambda x: int(x), input("Key in Net Income of the format 'Current Year - 3 Current Year - 2 Current Year - 1': ").split()))
+netIncome = list(map(lambda x: float(x), input("Key in Net Income of the format 'Current Year - 3 Current Year - 2 Current Year - 1': ").split()))
 
 # Calculate Free Cash Flow to Net Income Ratio
 FCF_NI_ratios = []
 for i in range(len(cashFlow)):
+    if (i + 1 == len(cashFlow)):
+        break
     FCF_NI_ratios.append(netIncome[i + 1] / cashFlow[i])
 FCF_NI = min(FCF_NI_ratios)
 
 # Prompt for Total Revenue (Can be obtained on Yahoo Finance)
-totalRevenue = input("Key in Total Revenue of the format 'Current Year - 3 Current Year - 2 Current Year - 1 Analyst Estimates': ").split()
+totalRevenue =  list(map(lambda x: float(x), input("Key in Total Revenue of the format 'Current Year - 3 Current Year - 2 Current Year - 1 Analyst Estimates': ").split()))
 
 # Calculate Revenue Growth Rate
 growthRates = []
 for i in range(len(totalRevenue)):
+    if (i + 1 == len(totalRevenue)):
+        break
     growthRates.append(totalRevenue[i + 1] / totalRevenue[i])
 growthRate = min(growthRates)
 
@@ -37,6 +41,8 @@ for i in range(YEARS):
 # Calculate Net Income Margins
 NI_margins = []
 for i in range(len(netIncome)):
+    if (i + 1 == len(netIncome)):
+        break
     NI_margins.append(netIncome[i + 1] / totalRevenue[i])
 NI_margin = 1 + min(NI_margins)
 
@@ -52,36 +58,36 @@ for i in reversed(range(1, YEARS + 1)):
 def weighedAverageCostToCaptial():
 
     # Prompt User for Interest Expense (Can be obtained from Income Statement on Yahoo Finance)
-    interestExpense = int(input("Enter Interest Expense from Income Statement: "))
+    interestExpense = float(input("Enter Interest Expense from Income Statement: "))
 
     # Prompt User for Current Debt (Can be obtained from Balance Sheet on Yahoo Finance)
-    currentDebt = int(input("Enter Current Debt from Balance Sheet: "))
+    currentDebt = float(input("Enter Current Debt from Balance Sheet: "))
 
     # Prompt User for Long Term Debt (Can be obtained from Balance Sheet on Yahoo Finance)
-    longTermDebt = int(input("Enter Long Term Debt from Balance Sheet: "))
+    longTermDebt = float(input("Enter Long Term Debt from Balance Sheet: "))
     totalDebt = currentDebt + longTermDebt
 
     debtRate = interestExpense / totalDebt
     
-    incomebeforeTax = int(input('Enter Income Before Text from Income Statement: '))
-    incomeTaxExpense = int(input('Enter Income Tax Expense from Income Statement: '))
+    incomebeforeTax = float(input('Enter Income Before Tax from Income Statement: '))
+    incomeTaxExpense = float(input('Enter Income Tax Expense from Income Statement: '))
     effectiveTaxRate = incomeTaxExpense / incomebeforeTax
     costofDebt = debtRate * (1 - effectiveTaxRate)
 
     def capitalAssetPricingModel():
-        riskFreeRate = int(input('Enter 10 Year US Treasury Bond Rates from Yahoo Finance as Risk Free Rate: '))
-        beta = int(input('Enter Beta Value from Yahoo Finance: '))
-        expectedMarketReturn = int(input('Enter the expected Market Return (10 Yr Average return of S&P500): '))
+        riskFreeRate = float(input('Enter 10 Year US Treasury Bond Rates from Yahoo Finance as Risk Free Rate: '))
+        beta = float(input('Enter Beta Value from Yahoo Finance: '))
+        expectedMarketReturn = float(input('Enter the expected Market Return (10 Yr Average return of S&P500): '))
         return riskFreeRate + beta * (expectedMarketReturn - riskFreeRate)
 
-    marketCap = int(input('Enter Market Cap of company: '))
+    marketCap = float(input('Enter Market Cap of company: '))
     weightDebt = round(totalDebt / (totalDebt + marketCap))
     weightEquity = round(marketCap / (totalDebt + marketCap))
     return weightDebt * costofDebt + weightEquity * capitalAssetPricingModel()
 
 rateOfReturn = weighedAverageCostToCaptial()
-sharesOutstanding = int(input('Enter Shares Outstanding from 10K: '))
-perpetualGrowthRate = int(input('Enter Perpetual Growth Rate in percent (2.5% is a good rate): ')) / 100
+sharesOutstanding = float(input('Enter Shares Outstanding from 10K (Note if the shares are in thousands and change to match data set): '))
+perpetualGrowthRate = float(input('Enter Perpetual Growth Rate in percent (2.5% is a good rate): ')) / 100
 
 # Calculate the Free Cash Flow at the END of the 4th Year Projection
 terminalValue = totalRevenue[-1] * (1 + perpetualGrowthRate) / (rateOfReturn - perpetualGrowthRate)
@@ -91,7 +97,19 @@ discountFactor = []
 
 # Current Year cash flow info is at index 2
 for i in range(2, len(cashFlow)):
+
+    # Add Discount Factor for Terminal value which is the same as the discount factor of the last year
+    if (i == (len(cashFlow) - 1)):
+        discountFactor.append((1 + rateOfReturn) ** (i - 1))
+        continue
+
     discountFactor.append((1 + rateOfReturn) ** i)
 
+todaysValue = 0
+for i in range(2, len(cashFlow)):
+    todaysValue += cashFlow[i] / discountFactor[i - 2]
 
+intrinsicValue = todaysValue / sharesOutstanding
+print(f'''#############################################
+INTRINISC VALUE OF {company} is: {intrinsicValue}''')
 
